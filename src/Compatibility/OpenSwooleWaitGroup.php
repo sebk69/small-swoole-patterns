b4e589e7522d9da6f0352d488520844b12390014
Sébastien Kus <k.sebastien@la-becanerie.com>
2022-12-22 18:55:54 +0100

    Swoole copyright string

--- src/Compatibility/OpenSwooleWaitGroup.php
+++ src/Compatibility/OpenSwooleWaitGroup.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * This file is part of Swoole.
+ *
+ * @link     https://www.swoole.com
+ * @contact  team@swoole.com
+ * @license  https://github.com/swoole/library/blob/master/LICENSE
+ */
+
 namespace Sebk\SmallSwoolePatterns\Compatibility;
 
 use Swoole\Coroutine\BadMethodCallException;
